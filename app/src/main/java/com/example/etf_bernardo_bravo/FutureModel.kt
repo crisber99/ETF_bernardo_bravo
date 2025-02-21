4c111b70763467a9d208d41b05316d916dbc1ea1
Crisber99 <bcbs1986@gmail.com>
2025-02-21 01:26:47 -0300

    actualización 21-02-2025 V2

---
+++ app/src/main/java/com/example/etf_bernardo_bravo/FutureModel.kt
@@ -0,0 +1,9 @@
+package com.example.etf_bernardo_bravo
+
+data class FutureModel(
+    val day:String,
+    val picPath:String,
+    val status:String,
+    val highTemp:Int,
+    val lowTemp:Int
+)
---
+++ app/src/main/java/com/example/etf_bernardo_bravo/HourlyModel.kt
@@ -0,0 +1,8 @@
+package com.example.etf_bernardo_bravo
+
+data class HourlyModel(
+    val hour:String,
+    val temp:Int,
+    val picPath:String
+
+)
